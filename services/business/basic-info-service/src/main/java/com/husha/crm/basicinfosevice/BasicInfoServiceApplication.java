d35c6080dd65c700575498483176907da6504ce3
alalbd <Hush@66915541>
2020-08-08 15:59:10 +0430

    init proje

---
+++ services/business/basic-info-service/src/main/java/com/husha/crm/basicinfosevice/BasicInfoServiceApplication.java
@@ -0,0 +1,15 @@
+package com.husha.crm.basicinfosevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class BasicInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicInfoServiceApplication.class, args);
+	}
+
+}
---
+++ services/business/basic-info-service/src/test/java/com/husha/crm/basicinfosevice/BasicInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.husha.crm.basicinfosevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
